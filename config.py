bb82915d36678cb59d7fc57b90493b8e9c170356
smzht <smzhts1@gmail.com>
2026-09-13 20:58:44 +0900

    version を更新

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 ##  Windows の操作を Emacs のキーバインドで行うための設定（Keyhac版）
 #########################################################################
 
-fakeymacs_version = "20260823_01"
+fakeymacs_version = "20260913_01"
 
 import time
 import os
